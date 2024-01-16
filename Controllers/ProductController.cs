acdc254aa4fd037ba9baec8ee408fe49cf79328a
Mert Aktas <mert_aktas07@outlook.com>
2024-01-16 22:11:40 +0300

    16/01/24

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme_WİHTOOP__Product.Controllers
+    {
+    public class ProductController : Controller
+        {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        public IActionResult Index()
+            {
+            var values = productManager.TGetList();
+            return View(values);
+            }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+            {
+            return View();
+            }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+            {
+            productManager.TInsert(p);
+            return RedirectToAction("Index");
+            }
+        }
+    }
