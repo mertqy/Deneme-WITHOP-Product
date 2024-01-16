8e7380cca22da1fe4496941b018c7fefbd311d45
Mert Aktas <mert_aktas07@outlook.com>
2024-01-16 12:21:38 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Deneme_WİHTOOP__Product.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        }
+    }
